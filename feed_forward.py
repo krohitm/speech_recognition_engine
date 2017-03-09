3b27b97b012d43c6dcc7dcb77b32f6cb0e7a56b2
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-03-08 20:37:10 -0500

    updating cells and hidden states

--- feed_forward.py
+++ feed_forward.py
@@ -9,16 +9,13 @@ np.set_printoptions(threshold=np.nan)
 
 class train_rnn_model(object):
 
-    def __init__(self, epochs = 10):
-        # self.input_data = input_data
-        # self.output_data = output_data
+    def __init__(self, epochs):
         self.epochs = epochs
 
-    def gate(self, x, wx, wh, hidden_state_prev, bias, gate_type):
+    def gate(self, x, w, hidden_state_prev, bias, gate_type):
         """
         :param x: input sample to the layer
-        :param wx: weight matrix wx for the hidden layer
-        :param wh: hidden state weight matrix for the hidden layer
+        :param w: weight matrix for the hidden layer
         :param hidden_state_prev: previous hidden states of the hidden layer
         :param bias: bias as per the gate for which values are being calculated
         :param gate_type: defines gate type (hidden, output, forget)
@@ -28,23 +25,30 @@ class train_rnn_model(object):
         key_h = 'w_h' + gate_type
 
         if gate_type == 'c':
-            gate_output = self.tanh(np.dot(np.transpose(wx[key_x]), x) + np.squeeze(np.dot(np.transpose(wh[key_h]),
-                                                                                   hidden_state_prev) +bias[gate_type]))
+            gate_output = self.tanh(np.dot(np.transpose(w[key_x]), x) +
+                                    np.squeeze(np.dot(np.transpose(w[key_h]), hidden_state_prev) +bias[gate_type]))
         else:
-            gate_output = self.sigmoid(np.dot(np.transpose(wx[key_x]), x) + np.squeeze(np.dot(np.transpose(wh[key_h]),
-                                                                                   hidden_state_prev) +bias[gate_type]))
+            gate_output = self.sigmoid(np.dot(np.transpose(w[key_x]), x) +
+                                       np.squeeze(np.dot(np.transpose(w[key_h]), hidden_state_prev) +bias[gate_type]))
+        return gate_output
+
+    def cell_state(self, FG, cell_states_prev, input_gate, input, weights, hidden_states_prev, bias):
+        gate_output = (FG * np.squeeze(cell_states_prev)) + ( input_gate * np.squeeze(self.gate(input, weights,
+                                                                                  hidden_states_prev, bias, 'c')))
         return gate_output
 
     def sigmoid(self, z):
         """z: net input from previous layer
         a: sigmoid activation of net input"""
-        a = 1.0/(1.0+np.exp(-z))
+        a = 1.0/(1.0+np.exp(-1.0 * z))
         return a
     
     def tanh(self, z):
         """z: net input from previous layer
         a: tanh activation of input"""
-        a = (np.exp(-z) - np.exp(-z))/(np.exp(-z) + np.exp(-z))
+        #num = np.exp(-z) - np.exp(-z)
+        #den = np.exp(-z) + np.exp(-z)
+        a = (np.exp(z) - np.exp(-z))/(np.exp(z) + np.exp(-z))
         # log.info(a)
         return a
 
@@ -53,7 +57,7 @@ class train_rnn_model(object):
         return sf
     
     def feedforward(self, xi, MBsize, weights, hidden_states_prev, cell_states_prev, bias):
-        """
+        """f
         :param xi: input audio signals batch in the form of spectrogram
         :param weights: weights of the layers in a dictionary
         :param hidden_states_prev: previous hidden states of the hidden layers
@@ -86,9 +90,11 @@ class train_rnn_model(object):
 
         for layer in hidden_layers:
             # initializing gates for hidden layers
-            temp_array = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
-            input_gate[layer] = forget_gate[layer] = cell_state_gate[layer] = \
-                output_gate[layer] = hidden_state[layer] = temp_array
+            input_gate[layer] = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
+            forget_gate[layer] = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
+            cell_state_gate[layer] = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
+            output_gate[layer] = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
+            hidden_state[layer] = np.zeros((num_samples, hidden_states_prev[layer].shape[0]))
 
         # calculating gate values of samples
         for file_num in range(MBsize):
@@ -97,31 +103,36 @@ class train_rnn_model(object):
                 input = xi[file_num, sample_num,:]
                 for layer in hidden_layers:
                     # x, wx, wh, hidden_state_prev, bias
-                    input_gate[layer][sample_num] = self.gate(input, weights[layer], weights[layer],
-                                                              hidden_states_prev[layer], bias[layer], 'i')
-                    forget_gate[layer][sample_num] = self.gate(input, weights[layer], weights[layer],
-                                                              hidden_states_prev[layer], bias[layer], 'f')
-                    cell_state_gate[layer][sample_num] = (forget_gate[layer][sample_num] *
-                                                          np.squeeze(cell_states_prev[layer]))\
-                                                         + (input_gate[layer][sample_num] *
-                                                            np.squeeze(self.gate(input, weights[layer], weights[layer],
-                                                                                 hidden_states_prev[layer], bias[layer],
-                                                                                 'c')))
-                    output_gate[layer][sample_num] = self.gate(input, weights[layer], weights[layer],
-                                                              hidden_states_prev[layer], bias[layer], 'o')
+                    input_gate[layer][sample_num] = self.gate(input, weights[layer], hidden_states_prev[layer],
+                                                              bias[layer], 'i')
+                    forget_gate[layer][sample_num] = self.gate(input, weights[layer], hidden_states_prev[layer],
+                                                               bias[layer], 'f')
+                    cell_state_gate[layer][sample_num] = self.cell_state(
+                        forget_gate[layer][sample_num], cell_states_prev[layer], input_gate[layer][sample_num] ,
+                        input, weights[layer], hidden_states_prev[layer], bias[layer])
+                    output_gate[layer][sample_num] = self.gate(input, weights[layer], hidden_states_prev[layer],
+                                                               bias[layer], 'o')
                     hidden_state[layer][sample_num] = output_gate[layer][sample_num] *\
                                                       self.tanh(cell_state_gate[layer][sample_num])
+                    #log.info(hidden_state[layer][sample_num])
                     # input for next layer
                     input = hidden_state[layer][sample_num]
+                    hidden_states_prev[layer] = np.expand_dims(hidden_state[layer][sample_num], axis = 1)
+                    cell_states_prev[layer] = np.expand_dims(cell_state_gate[layer][sample_num], axis = 1)
+                    #print hidden_states_prev[layer].shape
+                    #print cell_states_prev[layer].shape
+                    #return
+
 
                 output = np.dot(np.transpose(weights['output']), np.expand_dims(input, axis = 1)) + bias['y']
+                #log.info(output)
                 y_softmax.append(self.softmax(output))
                 y.append(output)
 
         y = np.array(y)
         y_softmax = np.array(y_softmax)
         # print y
-        # log.info(y_softmax)
+        log.info(y_softmax)
         # return
         return input_gate, forget_gate, cell_state_gate, output_gate, hidden_state, y, y_softmax
 
@@ -143,10 +154,6 @@ class train_rnn_model(object):
         """
 
         weights, hidden_states_prev, cell_states_prev, bias = initialize_weights.main()
-        # print hidden_states.keys()
-        # print hidden_states['hidden2'].shape
-        # return
-        flag = 0
         for epoch in range(epochs):
             for i, batch in enumerate(datagen.iterate_dev(MBsize, sortBy_duration = True)):
                 xi = batch['x']
@@ -154,33 +161,29 @@ class train_rnn_model(object):
                 text = batch['texts']
                 input_lengths = batch['input-lengths']
                 label_lengths = batch['label-lengths']
-                # print xi.shape
-                # print label_lengths
-                flag += 1
-                # if flag >= 2:
-                # return
-                input_gate, forget_gate, cell_state_gate, output_gate, hidden_state, y, y_softmax = \
-                self.feedforward(xi, MBsize, weights, hidden_states_prev, cell_states_prev, bias)
+                input_gate, forget_gate, cell_state_gate, output_gate, hidden_state, y, y_softmax =\
+                    self.feedforward(xi, MBsize, weights, hidden_states_prev, cell_states_prev, bias)
                 # print y_softmax.shape
-                log.info(np.array_str(self.labels(y_softmax)))
-                # print y
+                #log.info(np.array_str(self.labels(y_softmax)))
+                #log.info(y)
+                return
                 # return
 
 
 def main(dev_desc_file, mini_batch_size, epochs):
     datagen = DataGenerator()
     datagen.load_dev_data(dev_desc_file)
-    model = train_rnn_model()
+    model = train_rnn_model(epochs)
     trained_rnn_model = model.rnn_lstm_model_train(datagen, mini_batch_size, epochs)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dev_desc_file', type=str,
-                        help='Path where JSON file for transcripts and audio files is stored')
-    parser.add_argument('--mini_batch_size', type=int, default=50,
+    parser.add_argument('--dev_desc_file', type=str,
+                        help='Path where JSON file for transcripts and audio files is stored', default = 'data.json')
+    parser.add_argument('--mini_batch_size', type=int, default=1,
                         help='Size of mini batch for training')
-    parser.add_argument('--epochs', type=int, default=10, help='No. of epochs to train the model')
+    parser.add_argument('--epochs', type=int, default=1, help='No. of epochs to train the model')
     args = parser.parse_args()
 
     main(args.dev_desc_file, args.mini_batch_size, args.epochs)
